04b4c69b9b1a682a8ef4d79619e71f2438e1f0ee
shuihan <gaojian0614@163.com>
2018-12-11 14:02:33 +0800

    Initial Commit

---
+++ pages/history/history.js
@@ -0,0 +1,89 @@
+// pages/history.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    array: [{
+      time:'1分钟前',
+      descr: 'today do sth',
+    }, {
+      time:"昨天",
+        descr: 'today do sth'
+    }],
+    animation:""
+  },
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    this.animation = wx.createAnimation({
+      duration: 1400,
+      timingFunction: 'linear', // "linear","ease","ease-in","ease-in-out","ease-out","step-start","step-end"
+      delay: 0,
+      transformOrigin: '50% 50% 0',
+      success: function (res) {
+        console.log("res")
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  getDetail: function (e) {
+    this.animation.rotate(180 * (1)).step()
+    this.setData({
+      animation: this.animation.export()
+    })
+
+  }
+})
